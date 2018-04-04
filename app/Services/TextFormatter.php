64ce6e1e21019d018ba9879b85b060b2e8d2ef31
alanmanderson <alanmanderson@gmail.com>
2018-04-03 18:24:31 -0600

    Use randomizer for text

--- app/Services/TextFormatter.php
+++ app/Services/TextFormatter.php
@@ -5,8 +5,15 @@ use Illuminate\Database\Eloquent\Collection;
 
 class TextFormatter{
     public function format(string $template, Collection $inputs){
-        foreach($inputs as $input){
+        $counter = 1;
+        while(true){
+            $curVals = $inputs->filter(function ($value, $Key) use ($counter){
+                return $value->prompt->ordinal_rank == $counter;
+            });
+            if ($curVals->count() == 0) break;
+            $input = $curVals->random(1)->first();
             $template = str_replace('{{'.$input->prompt->ordinal_rank.'}}', $input->text, $template);
+            $counter++;
         }
         return $template;
     }
